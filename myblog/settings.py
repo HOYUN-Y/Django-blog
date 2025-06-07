cc71946cda56bbd78d67b37134d3e2684e113854
HOYUN-Y <hoyun0131@me.com>
2025-06-08 04:36:24 +0900

    debug setting to False

--- myblog/settings.py
+++ myblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+k^--#0&-a^evz8adkwdf_ui_-5)$@-!$5gbrql_$ov%iopoy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.ec2-13-125-208-9.ap-northeast-2.compute.amazonaws.com']
 
